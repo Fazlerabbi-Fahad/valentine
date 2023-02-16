d8607add64b7db8ada5a83c62273d5e4a6ab98d0
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2023-02-16 17:04:24 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-var button = document.getElementById("bouncing");
+const button = document.getElementById("bouncing");
 
-button.addEventListener("click", function () {
+
+button.addEventListener("mouseover", function () {
     var changeTop = (Math.random() * 500);
     var changeLeft = (Math.random() * 500);
+
+
     $("button").css("margin-top", changeTop + "px");
     $("button").css("margin-left", changeLeft + "px");
 });
+
